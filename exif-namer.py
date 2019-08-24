031eb8ced621b67fc748780ee72abf76fc203a90
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2019-08-24 21:01:58 +0300

    Deep refactoring, change exif package

--- exif-namer.py
+++ exif-namer.py
@@ -1,70 +1,76 @@
-import glob
-import os
+from argparse import ArgumentParser
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict
+import concurrent.futures
 import sys
 
-import exifread
+from exif import Image
 
 
-def make_cwd(args):
-    """Составляет из аргументов командной строки путь к желаемому каталогу"""
-    path = ""
-    for i in range(1, len(args), 1):
-        path += args[i]
-        if i != len(args)-1:
-            path += ' '
-    return path
+EXIF_DATETIME_ORIGINAL_TAG = "EXIF DateTimeOriginal"
 
 
-def process_directory(work_directory, file_type):
-    """Принимает на вход каталог, возвращает список *.file_type файлов"""
-    os.chdir(work_directory)
-    return glob.glob("*." + file_type)
+def get_args():
+    parser = ArgumentParser(description="EXIF namer script")
+    parser.add_argument("input_path", type=str,
+                        help="Path to image or directory with images")
+    parser.add_argument("output_dir", type=str,
+                        help="Path to output directory")
+    return parser.parse_args()
 
 
-def make_data_pattern(date_time):
-    """Принимает на вход дату и меняет её под шаблон"""
-    date_time = date_time.replace(":", "")
-    date_time = date_time.replace(" ", "_")
-    date_time = "IMG_" + date_time
-    return date_time
-
-
-def process_photo(cur_photo, cwd):
-    """Извлекает из фото exif-данные, на основе их переименовывает"""
-    image = open(cur_photo, "rb")
-    metadata = exifread.process_file(image)
-    image.close()
-    renamed = 0
-    if 'EXIF DateTimeOriginal' in metadata:
-        tag_value = metadata.get("EXIF DateTimeOriginal")
-        dt_pattern = make_data_pattern(tag_value.printable)
-        if not os.path.isfile(dt_pattern + '.jpg'):
-            os.rename(cur_photo, dt_pattern + '.jpg')
-            print(cur_photo + " -> " + dt_pattern + '.jpg')
+def process_image(in_img_path: Path, out_img_dir: Path) -> Dict[Path, Path]:
+    """ Extract EXIF data from image and return path mapping """
+    with open(in_img_path, "rb") as image_file_stream:
+        image = Image(image_file_stream)
+        if image.has_exif:
+            out_img_name = make_name(image.datetime_original)
+            out_img_path = out_img_dir.joinpath(out_img_name)
         else:
-            for i in range(1, 10, 1):
-                if not os.path.isfile(dt_pattern + "_(" + str(i)+ ")" + '.jpg'):
-                    os.rename(cur_photo, dt_pattern + "_(" + str(i) + ")" + '.jpg')
-                    print(cur_photo + " -> " + dt_pattern + "_(" + str(i)+ ")" + '.jpg')
-                    break
-        renamed = 1
-    else:
-        print(cur_photo + " -> " + "не переименовано")
-    return renamed
+            # if no EXIF datetime, not change initial name
+            out_img_path = out_img_dir.joinpath(in_img_path.name)
+    return {in_img_path: out_img_path}
 
 
-def main():
-    cwd_name = make_cwd(sys.argv)
-    photos = process_directory(cwd_name, "jpg")
-    print("Рабочий каталог: ", cwd_name)
-    pr_pics = 0
-    for photo in photos:
-        pr_pics += process_photo(photo, cwd_name)
-    print("\nВсего изображений:\t", len(photos))
-    print("Переименовано:\t\t", pr_pics)
-    print("Не переименовано:\t", len(photos)-pr_pics)
+def make_name(datetime_: str) -> str:
+    datetime_ = datetime_.replace(":", "")
+    datetime_ = datetime_.replace(" ", "_")
+    return f"IMG_{datetime_}.jpg"
+
+
+def rename_images(mappings: Dict[Path, Path]):
+    print("Mapping images...")
+    repeats = defaultdict(int)
+    for in_img_path, out_img_path in mappings.items():
+        if out_img_path.exists():
+            repeats[out_img_path] += 1
+            out_img_name = f"{out_img_path.stem}_" \
+                           f"({repeats[out_img_path]})" \
+                           f"{out_img_path.suffix}"
+            out_img_path = out_img_path.parent.joinpath(out_img_name)
+        print(f"{in_img_path} -> {out_img_path}")
+        out_img_path.write_bytes(in_img_path.read_bytes())
 
 
 if __name__ == "__main__":
-    print("EXIF-namer")
-    main()
+    args = get_args()
+
+    input_path = Path(args.input_path)
+    if not input_path.exists():
+        print(f"Path '{input_path}' not exists. Exit")
+        sys.exit(1)
+
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    if input_path.is_file():
+        mappings = process_image(input_path, output_dir)
+    elif input_path.is_dir():
+        mappings = {}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            exif_futures = [executor.submit(process_image, img, output_dir) for img in input_path.iterdir()]
+            for future in concurrent.futures.as_completed(exif_futures):
+                mappings.update(future.result())
+
+    rename_images(mappings)
